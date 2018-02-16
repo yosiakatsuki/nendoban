b363814ffc3fd549b32a5ca5423ed14f8a56d33c
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-02-16 18:09:53 +0900

    add js enqueue

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,6 +16,7 @@ function nendoban_scripts() {
 	wp_deregister_script('jquery');
 	wp_enqueue_script('jquery', nendoban_get_library_file_uri( '/jquery/jquery.min.js' ), array(), '3.3.1');
 	wp_enqueue_script( 'bootstrap-script', nendoban_get_library_file_uri( '/bootstrap-4/js/bootstrap.min.js' ), array('jquery'), false, true );
+	wp_enqueue_script( 'nendoban-script', get_template_directory_uri() . '/js/theme.bundle.js', array('jquery'), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
